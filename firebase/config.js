3679323c7d86ec51c5c1009dd3153cd838cfc015
Viam26 <medicalviam@gmail.com>
2026-09-20 13:07:22 -0600

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -2,10 +2,13 @@
    Origen: Firebase Console → Project settings → Your apps → Web. */
 
 window.VIAM_FIREBASE = {
-  apiKey: "PEGA_AQUI_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxx",
+  apiKey: "AIzaSyA4p_Y8dHIbfFyML4xqc8GLwG-yhqbiCHc",
+  authDomain: "viam-e93e0.firebaseapp.com",
+  projectId: "viam-e93e0",
+  storageBucket: "viam-e93e0.firebasestorage.app",
+  messagingSenderId: "396072652863",
+  appId: "1:396072652863:web:9135a5ce389c3fd629288a",
+  measurementId: "G-R9LBTTSH59"
 };
+
+
